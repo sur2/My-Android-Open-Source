31a4dec8105d82b70881d1657bab94c2535b7696
JEONG PYO YONG <sur2@naver.com>
2021-10-29 16:01:00 +0900

    Timer Test, TimerTask는 재사용 불가능, 인스턴스를 새로 생성할 것

--- NumberPickerTest/app/src/main/java/com/example/numberpickertest/MainActivity.java
+++ NumberPickerTest/app/src/main/java/com/example/numberpickertest/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.numberpickertest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.NumberPicker;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int MAX_HOUR = 99;
     private int MAX_MINUTE = 59;
@@ -15,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
     private MyNumberPicker npHour;
     private MyNumberPicker npMinute;
     private MyNumberPicker npSecond;
+
+    private Button btStopAndGo;
+
+    private Timer timer = null;
+    private TimerTask timerTask = null;
+
+    private boolean isStop = false;
+
+    private int totalNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +46,39 @@ public class MainActivity extends AppCompatActivity {
         npMinute.setMaxValue(MAX_MINUTE);
         npSecond.setMaxValue(MAX_SECOND);
 
+        btStopAndGo = findViewById(R.id.bt_stopAndGo);
+        btStopAndGo.setOnClickListener(this);
+        isStop = false;
+
+        timer = new Timer();
+        timerTask = new TimerTask() {
+            int count = 0;
+            @Override
+            public void run() {
+                Log.d("Timer", "count : " + count++);
+            }
+        };
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == btStopAndGo.getId()) {
+            if (isStop == false) {
+                isStop = true;
+                btStopAndGo.setText("Stop");
+                timer.schedule(timerTask, 0, 1000);
+            }
+            else {
+                isStop = false;
+                btStopAndGo.setText("Go");
+                timer.cancel();
+            }
+            Toast.makeText(this, "Total " + getTotalNumber(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public int getTotalNumber() {
+        return npHour.getValue() * 3600 + npMinute.getValue() * 60 + npSecond.getValue();
+    }
 
 }
